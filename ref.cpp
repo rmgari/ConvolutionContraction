1f4d12283d13fb82a4a4bcfde0af3f89255d886d
Rahul Garikapati <rgmhv08@cs.utexas.edu>
2024-10-28 14:56:23 -0500

    handling stride and padding with contraction

--- ref.cpp
+++ ref.cpp
@@ -15,7 +15,7 @@ Tensor naive_algo(int inc, int outc, Tensor input, Tensor kernel, Tensor b, int 
     // create the convolution layer with all given specifications
     nn::Conv2d layer = nn::Conv2d(nn::Conv2dOptions(inc, outc, k_size).stride(s).padding(p));
     layer->weight = kernel;
-    layer->bias = b;
+    // layer->bias = b;
     // perform the convolution
     return layer->forward(input);
 }
@@ -77,103 +77,77 @@ int main() {
     specs[3] = make_pair(1, 1);
     specs[4] = make_pair(1, 1);    
     
-    // for (int i = 0; i < kernels.size(); ++i) {
-    //     int outc = kernels[i].sizes()[0];
-    //     int inc = kernels[i].sizes()[1];
+    for (int layer = 0; layer < kernels.size(); ++layer) {
+        int Co = kernels[layer].sizes()[0];
+        int Ci = kernels[layer].sizes()[1];
+        int Wi = inputs[layer].sizes()[2], Hi = inputs[layer].sizes()[3];
+        int Wf = kernels[layer].sizes()[2], Hf = kernels[layer].sizes()[3];
+        int s = specs[layer].first, p = specs[layer].second;
+        int Wo = ((Wi - Wf + 2 * p) / s) + 1, Ho = ((Hi - Hf + 2 * p) / s) + 1;
 
-    //     Tensor res = naive_algo(inc, outc, inputs[i], kernels[i], biases[i], specs[i].first, specs[i].second);
-    //     // Tensor res = naive_algo(inc, outc, inputs[i], kernels[i], biases[i], 1, 1);
+        Tensor res = naive_algo(Ci, Co, inputs[layer], kernels[layer], biases[layer], s, p);      
 
-    //     // Source: https://stackoverflow.com/questions/73902752/how-can-i-get-the-maximum-values-of-a-tensor-along-a-dimension
-    //     cout << "Max diff: " << (torch::max(torch::abs(layer_outputs[i] - res))).item() << '\n';
-    // }
+        tblis::tensor<float> A = varray({Ci, Wi + 2 * p, Hi + 2 * p}, 0);
+        tblis::tensor<float> B = varray({Ci, Co, Wf, Hf}, 0);
+        tblis::tensor<float> C = varray({Co, Wo, Ho}, 0);
 
-    // cout << '\n';
-
-    // setup algo 2
-    int Wi=5, Hi=5, Ci=1;
-    int Wf=3, Hf=3;
-    // assume stride = 1, padding = 0
-    int Wo = Wi - Wf + 1, Ho = Wi - Wf + 1, Co = 1;
-
-    // Tensor a2_input = torch::empty({Ci, Wi, Hi});
-    // int fill = 1;
-    // for (int i = 0; i < Wi; ++i) {
-    //     for (int j = 0; j < Hi; ++j) {
-    //         a2_input[0][i][j] = fill++;
-    //     }
-    // }
-    
-    // Tensor a2_kernel = torch::empty({Co, Ci, Wf, Hf});
-    // fill = 1;
-    // for (int i = 0; i < Wf; ++i) {
-    //     for (int j = 0; j < Hf; ++j) {
-    //         a2_kernel[0][0][i][j] = fill++;
-    //     }
-    // }
-
-    Tensor a2_input = torch::randn({Ci, Wi, Hi});
-    Tensor a2_kernel = torch::randn({Co, Ci, Wf, Hf});
-
-    Tensor c_ref = naive_algo(Ci, Co, a2_input, a2_kernel, torch::zeros(Co), 1, 0);
-    
-    // vector<len_type> dimsA = {Ci, Wi, Hi};
-    // tblis::tensor<float> A(dimsA, COLUMN_MAJOR); 
-    tblis::tensor<float> A = varray({Ci, Wi, Hi}, 0);    
-    tblis::tensor<float> B = varray({Ci, Co, Wf, Hf}, 0);
-    tblis::tensor<float> C = varray({Co, Wo, Ho}, 0);
-
-    // copy over A, B
-    for (int i = 0; i < Ci; ++i) {
-        for (int j = 0; j < Wi; ++j) {
-            for (int k = 0; k < Hi; ++k) {
-                A(i, j, k) = a2_input[i][j][k].item<float>();
-            }
-        }
-    }
-
-    for (int i = 0; i < Co; ++i) {
-        for (int j = 0; j < Ci; ++j) {
-            for (int k = 0; k < Wf; ++k) {
-                for (int l = 0; l < Hf; ++l) {
-                    B(j, i, k, l) = a2_kernel[i][j][k][l].item<float>();
+        // copy inputs into A
+        for (int i = 0; i < Ci; ++i) {
+            for (int j = p; j < Wi + p; ++j) {
+                for (int k = p; k < Hi + p; ++k) {
+                    A(i, j, k) = inputs[layer][0][i][j - p][k - p].item<float>();
                 }
             }
-        }
-    }    
+        }       
 
-    // writing algo 2
-    for (int l = 0; l < Ho; ++l) {
-        for (int n = 0; n < Hf; ++n) {
-            for (int m = 0; m < Wf; ++m) {
-                for (int i = 0; i < Ci; ++i) {
-                    for (int k = 0; k < Wo; ++k) {
-                        for (int j = 0; j < Co; ++j) {
-                            C(j, k, l) += A(i, k + m, l + n) * B(i, j, m, n);
-                        }
+        // copy kernel into B
+        for (int i = 0; i < Co; ++i) {
+            for (int j = 0; j < Ci; ++j) {
+                for (int k = 0; k < Wf; ++k) {
+                    for (int l = 0; l < Hf; ++l) {
+                        B(j, i, k, l) = kernels[layer][i][j][k][l].item<float>();
                     }
                 }
             }
         }
+        // fix kernel, output -> determine correct index mapping in input  
+        // second and fourth by s
+        tblis::tensor<float> A2 = varray_view<float>({Ci, Wo, Wf, Ho, Hf}, A.data(), {(Wi + 2 * p) * (Hi + 2 * p), s * (Hi + 2 * p), (Hi + 2 * p), s, 1});
+        tblis::tensor<float> C2 = varray({Co, Wo, Ho}, 0);
+        mult<float>(1, A2, "abcde", B, "afce", 0, C2, "fbd");
+
+        // Source: https://stackoverflow.com/questions/73902752/how-can-i-get-the-maximum-values-of-a-tensor-along-a-dimension
+        // cout << "Max diff: " << (torch::max(torch::abs(layer_outputs[layer] - res))).item() << '\n';
+        // check max diff between res and C2
+
+        float max_abs_diff = 0;
+        for (int i = 0; i < Co; ++i) {
+            for (int j = 0; j < Wo; ++j) {
+                for (int k = 0; k < Ho; ++k) {
+                    max_abs_diff = max(max_abs_diff, abs(res[0][i][j][k].item<float>() - C2(i, j, k)));
+                }
+            }
+        }
+        cout << "Co\tCi\tWf\tHf\tWi\tHi\ts\tp\n";
+        cout << Co << '\t' << Ci << '\t' << Wf << '\t' << Hf << '\t' << Wi << '\t' << Hi << '\t' << s << '\t' << p << '\n';
+        cout << "Max diff: " << max_abs_diff << "\n\n";
     }
-
-    // testing algo 2
-    cout << A << '\n';
-    cout << B << '\n';
-    cout << c_ref << '\n';
-    cout << C << '\n';
-
-    // fix kernel, output -> determine correct index mapping in input    
     
-    tblis::tensor<float> A2 = varray_view<float>({Ci, Wo, Wf, Ho, Hf}, A.data(), {Wi * Hi, Hi, Hi, 1, 1});
-    // for (int i = 0; i < 3; ++i) {
-    //     for (int j = 0; j < 3; ++j) {
-    //         cout << A2(0, 0, i, 0, j) << ' ';
-    //     }
-    //     cout << '\n';
-    // }
-    tblis::tensor<float> C2 = varray({Co, Wo, Ho}, 0);
-    mult<float>(1, A2, "abcde", B, "afce", 0, C2, "fbd");
-    cout << C2 << '\n';    
     return 0;
 }
+
+    // Tensor c_ref = naive_algo(Ci, Co, a2_input, a2_kernel, torch::zeros(Co), s, p);
+    // // writing algo 2
+    // for (int l = 0; l < Ho; ++l) {
+    //     for (int n = 0; n < Hf; ++n) {
+    //         for (int m = 0; m < Wf; ++m) {
+    //             for (int i = 0; i < Ci; ++i) {
+    //                 for (int k = 0; k < Wo; ++k) {
+    //                     for (int j = 0; j < Co; ++j) {
+    //                         C(j, k, l) += A(i, k + m, l + n) * B(i, j, m, n);
+    //                     }
+    //                 }
+    //             }
+    //         }
+    //     }
+    // }
